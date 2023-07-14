716247e6fb577cc5edc11f91b909eb50d4652260
chamny20 <dge3179@ajou.ac.kr>
2023-07-14 16:09:57 +0900

    [unrated] Title: 둘만의 암호, Time: 2.42 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,10 @@
+function solution(s, skip, index) {
+    // 1) filter 함수를 사용하여 skip문자를 제거 배열을 반환 받는다.
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    const arr = alphabet.filter((a) => !skip.includes(a));
+    console.log(arr.length);
+    
+    // 2) map 함수를 사용하여  (현재 알파벳) + index에 위차한 값을 반환한다. 
+    return s.split('').map((a) => arr[(arr.indexOf(a)+index) % arr.length]).join('');
+}
