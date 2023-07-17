46808bc8af580e62cd765eb636402dd6cbfc8aa1
chamny20 <dge3179@ajou.ac.kr>
2023-07-18 01:05:05 +0900

    Update 대충 만든 자판.js

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,16 +1,17 @@
 function solution(keymap, targets) {
     const answer = new Array(targets.length).fill(-1);
     
-    // targets을 탐색
-    for(let i = 0; i < targets.length; i++) {
-        const target = targets[i];
-        let count = 0;
-        // target의 각 문자를 탐색
-        for(let j = 0; j < target.length; j++) {
-            const str = target[j];
-            // keymap의 각 요소 중 target[j]를 포함하는 문자의 minimum index 구함
-            const min = Math.min(...keymap.map(x => {
+    for(let i=0; i<targets.length; i++) {
+        let target = targets[i];
+        // console.log(target);
+        let count=0;
+        for(let j=0; j<target.length; j++) {
+            let str = target[j];
+            // console.log("keymap:", keymap);
+            let min = Math.min(...keymap.map(x => {
                 const idx = x.indexOf(str);
+                // console.log("here:", x.indexOf(str));
+                // 둘 중에 작은 것을 선택해야 함
                 return idx === -1 ? Infinity : idx + 1;
             }));
             if(min === Infinity) {
@@ -18,9 +19,10 @@ function solution(keymap, targets) {
                 break;
             }
             count += min;
-        }
-        answer[i] = count;
+            // console.log("count:", count);
+
     }
-    
+        answer[i] = count;
+}
     return answer;
 }
