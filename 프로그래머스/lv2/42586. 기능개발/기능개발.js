e10e467026446b5de8ca9f8aec0a1c64a043faab
Chaemin Lee <dge3179@ajou.ac.kr>
2023-09-05 23:50:37 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -6,7 +6,7 @@ function solution(progresses, speeds) {
     progresses.forEach((progress, idx) => {
         restDay.push(Math.ceil((100-progress) / speeds[idx]));
     })
-   
+    
     let maxDay = restDay[0];
     
     for (let i=1; i<restDay.length; i++) {
@@ -21,14 +21,3 @@ function solution(progresses, speeds) {
     answer.push(funcCnt);
     return answer;
 }
-
-/* 
-
-남은 progresses를 먼저 정하기.
-next
-며칠 동안 가능한지 day 수 정하기.
-next
-만약 [7,3,9]에서
-0번째값 > 1번째값 이면 cnt++(기능개수);
-if문 비교필요
-*/
