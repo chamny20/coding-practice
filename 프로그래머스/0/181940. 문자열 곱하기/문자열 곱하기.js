79113ee09dbcce3a3f96669f4ab53ae7ccef26be
Chaemin Lee <dge3179@ajou.ac.kr>
2024-06-16 14:44:26 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -0,0 +1,12 @@
+// function solution(my_string, k) {
+//     let answer='';
+    
+//     for (let i=0; i<k; i++) {
+//         answer += my_string
+//     }
+//     return answer;
+// }
+
+function solution(my_string, k) {
+    return my_string.repeat(k);
+}
