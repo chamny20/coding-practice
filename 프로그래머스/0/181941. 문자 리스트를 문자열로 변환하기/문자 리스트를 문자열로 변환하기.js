3b351363d4bc9dbb6929594447e23d27b3655055
Chaemin Lee <dge3179@ajou.ac.kr>
2024-07-02 22:31:27 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+//     let answer = '';
+    
+//     for (let i=0; i<arr.length; i++) {
+//         answer += arr[i];
+//     }
+    
+//     return answer;
+    return arr.join('');
+}
