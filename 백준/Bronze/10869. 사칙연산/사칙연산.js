eb48bcc4c1c435177a8c2ffa212741eda212154c
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-02 10:21:06 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const inputData = fs
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : "../../../../index.txt"
+  )
+  .toString()
+  .split(" ")
+  .map((value) => +value);
+
+const [a, b] = inputData;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
