3649c8e6c251f69cfca35df872a62af4f163b574
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-05 15:37:35 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().split('\n').map(x => Number(x));
+
+let max = input[0];
+let idx = 0;
+
+for (let i=0; i<9; i++) {
+    if (max < input[i]) {
+        max = input[i];
+        idx = i;
+    }
+       
+}
+
+console.log(max);
+console.log(idx+1);
