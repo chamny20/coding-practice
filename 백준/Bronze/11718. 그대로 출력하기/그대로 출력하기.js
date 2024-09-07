1000657d83059c8cc5b14bdb7a1036b88a71a871
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-07 17:18:03 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -1,5 +1,9 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-for (let i=0; i<input.length; i++) {
-    console.log(input[i]);
-}
+// for (let i=0; i<input.length; i++) {
+//    console.log(input[i]);
+// }
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+console.log(input);
