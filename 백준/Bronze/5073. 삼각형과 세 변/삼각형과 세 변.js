9b879fafa044801d4b5184516ebc6d690679332b
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-14 15:41:52 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 84 ms, Memory: 9408 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
@@ -1,6 +1,6 @@
 let input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
 
-for (let i=0; i<=input.length; i++) {
+for (let i=0; i<input.length; i++) {
     let [a, b, c] = input[i].split(" ").map(Number);
     if (a === 0 && b === 0 && c === 0) break;  // 입력 종료 조건
 
