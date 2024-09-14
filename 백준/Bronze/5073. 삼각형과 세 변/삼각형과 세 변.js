0ebe7a89f1e1fbe014b60d5bef5ab803aba2bf74
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-14 15:41:30 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 92 ms, Memory: 9404 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
@@ -0,0 +1,20 @@
+let input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
+
+for (let i=0; i<=input.length; i++) {
+    let [a, b, c] = input[i].split(" ").map(Number);
+    if (a === 0 && b === 0 && c === 0) break;  // 입력 종료 조건
+
+    let arr = [];
+    arr.push(a, b, c);
+    arr.sort((a,b) => a-b);
+    
+    if (arr[2] >= arr[0] + arr[1]) 
+        console.log("Invalid");
+    else if (arr[0] === arr[1] && arr[1]===arr[2])
+        console.log("Equilateral");
+    else if (arr[0] === arr[1] || arr[1] === arr[2] || arr[0] === arr[2])
+        console.log("Isosceles");
+    else
+        console.log("Scalene");
+}
+
