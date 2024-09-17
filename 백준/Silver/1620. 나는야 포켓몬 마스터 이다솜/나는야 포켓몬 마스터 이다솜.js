e4eab0e322cb4b8bf101e3fc5e7fa583a65da640
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-17 16:10:47 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 288 ms, Memory: 50872 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -4,23 +4,18 @@ const [n, m] = input[0].split(" ").map(Number);
 let namesMap = new Map();
 let ans = [];
 
-// 포켓몬 이름을 키로, 번호를 값으로 저장
-for (let i = 1; i <= n; i++) {
+for (let i=1; i<=n; i++) {
     namesMap.set(input[i], i);
 }
 
-for (let k = n + 1; k < input.length; k++) {
-    const query = input[k];
-
-    // query가 숫자인 경우 (포켓몬 번호로 이름 찾기)
-    if (!isNaN(Number(query))) {
-        // 숫자는 배열에서 바로 해당 번호의 포켓몬 이름을 가져옴
-        ans.push(input[Number(query)]);
-    } 
-    // query가 문자열인 경우 (포켓몬 이름으로 번호 찾기)
-    else {
-        ans.push(namesMap.get(query));
+for (let k=n+1; k<input.length; k++) {
+    if(!isNaN(Number(input[k]))) {
+        ans.push((input[Number(input[k])]));
+    } else {
+        let name = namesMap.get(input[k]);
+        ans.push(name);
     }
+    
 }
 
 console.log(ans.join("\n"));
