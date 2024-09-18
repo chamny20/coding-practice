eff800820497077be89be840cb9374924ba2680e
Chaemin Lee <dge3179@ajou.ac.kr>
2024-09-19 00:02:20 +0900

    [Silver III] Title: 분수 합, Time: 88 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.js
+++ 백준/Silver/1735. 분수 합/분수 합.js
@@ -3,6 +3,11 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("
 let [a, b] = input[0].split(" ").map(Number);
 let [c, d] = input[1].split(" ").map(Number);
 
+if (b % a === 0)
+    [a, b] = [1, b/a];
+if (d & c === 0)
+    [c, d] = [a, d/c];
+
 let A = a * d + b * c;
 let B = b * d;
 
