51f612df604885f324964681184e1915cf769a11
Chaemin Lee <dge3179@ajou.ac.kr>
2024-10-08 16:00:24 +0900

    [Silver III] Title: 후위 표기식2, Time: 88 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.js
@@ -0,0 +1,51 @@
+ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split("");
+let values = {};
+let strArr = arr.filter((el) => el !== '*' && el !== '/' && el !== '+' && el !== '-');
+let idx = 65;
+// { A: 1, B: 2, ...} 형태로 만들기
+for (let i=2; i<input.length; i++) {
+    // values[strArr[i-2]] = Number(input[i]);
+    values[String.fromCharCode(idx)] = Number(input[i]);
+    idx++;
+}
+
+let stack = [];
+for (let i=0; i<arr.length; i++) {
+    let str = arr[i];
+    let a, b;
+    
+    switch (str) {
+        case '+':
+            a = parseFloat(stack.pop());
+            b = parseFloat(stack.pop());
+            stack.push(b+a);
+            break;
+        case '-':
+            a = parseFloat(stack.pop());
+            b = parseFloat(stack.pop());
+            stack.push(b-a);
+            break;
+        case '*':
+            a = parseFloat(stack.pop());
+            b = parseFloat(stack.pop());
+            stack.push(b*a);
+            break;
+        case '/':
+            a = parseFloat(stack.pop());
+            b = parseFloat(stack.pop());
+            stack.push(b/a);
+            break;
+        default:
+            stack.push(values[str]);
+            break;
+    }
+}
+
+console.log(stack[0].toFixed(2));
+
+
+
+
