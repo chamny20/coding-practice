9bea2ccf0a34ff256bfd8bb8d9280b4c8ef5da89
Chaemin Lee <dge3179@ajou.ac.kr>
2024-11-06 02:18:22 +0900

    [level 2] Title: 주식가격, Time: 6.31 ms, Memory: 45.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,18 +1,18 @@
 function solution(prices) {
-    var ans = [];
+    let result = [];
     
     for (let i=0; i<prices.length; i++) {
         let cnt = 0;
+        
         for (let j=i+1; j<prices.length; j++) {
             cnt++;
             if (prices[i] > prices[j]) {
                 break;
             }
-            // console.log("cnt:", cnt)
+            // console.log(prices[i], prices[j], cnt);
         }
-        ans.push(cnt);
-        // console.log("ans:", ans)
+        result.push(cnt);
     }
-    
-    return ans;
+
+    return result;
 }
