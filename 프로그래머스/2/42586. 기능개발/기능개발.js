ee6ec4bb37f623b79094e31e39e7fbbb29cb591c
Chaemin Lee <dge3179@ajou.ac.kr>
2024-11-07 11:06:03 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,28 @@
 function solution(progresses, speeds) {
-    let arr = [];
     let ans = [];
-    let task = progresses.map((x) => 100 - x);
-    // console.log("task", task);
+    let remainArr = [];
     
-    let rest = task.map((x, idx) => Math.ceil(x / speeds[idx]));
+    for (let i=0; i<progresses.length; i++) {
+        let remain = Math.ceil((100 - progresses[i] ) / speeds[i]);
+        remainArr.push(remain);
+    }
     
+    let currentMax = remainArr[0]; // 현재 가장 앞에 있는 작업의 배포 기준 일 수
     let cnt = 1;
-    let maxDay = rest[0];
     
-    for (let i=1; i<rest.length; i++) {
-        if(rest[i] > maxDay) {
-            maxDay = rest[i];
+    for (let i = 1; i < remainArr.length; i++) {
+        if (remainArr[i] <= currentMax) {
+            cnt++;
+        } else {
             ans.push(cnt);
             cnt = 1;
-        } else {
-            cnt++;
+            currentMax = remainArr[i]; 
         }
     }
-    ans.push(cnt);
+    
+    ans.push(cnt)
+    
+    
+    
     return ans;
 }
