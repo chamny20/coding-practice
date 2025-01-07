f9149a7e5bb4ad3b74731222f4c76b7ad5d60b63
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-07 10:37:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,43 +1,33 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input.shift());
-const mapArr = input.map((v) => v.split("").map(Number));
-let visited = Array.from(Array(n), () => Array(n).fill(false));
-const dir = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-let ans = [];
+const dir = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+const maps = input.map((v) => v.split("").map(Number));
+const visited = Array.from(Array(n), () => Array(n).fill(false));
+let result = [];
 
-const bfs = (x, y) => {
-    const queue = [[x, y]];
-    visited[x][y] = true;
-    let cnt = 1;
-
-    while(queue.length) {
-        const [curX, curY] = queue.shift();
-
-        for (let i=0; i<4; i++) {
-            const nx = curX + dir[i][0];
-            const ny = curY + dir[i][1];
-            
-            if(nx>=0 && ny>=0 && nx<n && ny<n && mapArr[nx][ny] && !visited[nx][ny]) {
-                queue.push([nx, ny]);
-                cnt++;
-                visited[nx][ny] = true;
-            }
+const dfs = (start, visited) => {
+    let cnt = 1; // 집 개수
+    visited[start[0]][start[1]] = true;
+    
+    for (let i=0; i<dir.length; i++) {
+        const nx = start[0] + dir[i][0];
+        const ny = start[1] + dir[i][1];
+        
+        if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && maps[nx][ny]) {
+            cnt += dfs([nx, ny], visited); // 연결된 집을 재귀적으로 탐색하여 개수 누적
         }
     }
-    
     return cnt;
 }
 
-for (let i=0; i<n; i++) {
-    for (let j=0; j<n; j++) {
-        if (mapArr[i][j] && !visited[i][j]) {
-            ans.push(bfs(i, j));
-            visited[i][j] = true;
+for (let x=0; x<n; x++) {
+    for (let y=0; y<n; y++) {
+        if (!visited[x][y] && maps[x][y]===1) {
+            let cnt = dfs([x, y], visited);
+            result.push(cnt);
         }
-        
     }
 }
 
-
-console.log(ans.length);
-console.log(ans.sort((a,b) => a-b).join('\n'));
+console.log(result.length);
+console.log(result.sort((a,b) => a-b).join("\n"));
