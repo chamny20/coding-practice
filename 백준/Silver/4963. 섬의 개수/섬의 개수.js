c453147cbf1a0ba2ba873bd1342c4e8adb20f591
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-08 14:16:48 +0900

    [Silver II] Title: 섬의 개수, Time: 160 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.js
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.js
@@ -1,49 +1,42 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const dir = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+const result = [];
 
-const bfs = (start, maps, w, h, visited) => {
-    const queue = [start];
-    visited[start[0]][start[1]] = true;
+const dfs = (start, w, h, visited, maps) => {
+    const [x, y] = start;
+    visited[x][y] = true;
     
-    while (queue.length) {
-        const [curX, curY] = queue.shift();
+    for (let i = 0; i < dir.length; i++) {
+        const nx = x + dir[i][0];
+        const ny = y + dir[i][1];
         
-        for (let i=0; i<dir.length; i++) {
-            const nx = curX + dir[i][0];
-            const ny = curY + dir[i][1];
-            
-            if (nx>=0 && ny>=0 && nx<h && ny<w && !visited[nx][ny] && maps[nx][ny]) {
-                queue.push([nx, ny]);
-                visited[nx][ny] = true;
-            }
+        if (nx >= 0 && ny >= 0 && nx < h && ny < w && !visited[nx][ny] && maps[nx][ny]) {
+            dfs([nx, ny], w, h, visited, maps);
         }
-    }
+    }    
 }
 
-let i = 0;
-while (i < input.length) {
-    const [w, h] = input[i].split(" ").map(Number);
+while (input.length) {
+    const [w, h] = input.shift().split(" ").map(Number);
     if (w === 0 && h === 0) break;
     
     const maps = [];
-    for (let j=i+1; j<=h+i; j++) {
-        maps.push(input[j].split(' ').map(Number)); // maps 만들기
+    let count = 0;
+    
+    for (let i = 0; i < h; i++) {
+        maps.push(input.shift().split(" ").map(Number));
     }
-    
-    i += h+1;
-    
     const visited = Array.from(Array(h), () => Array(w).fill(false));
-    let count = 0; // 섬 개수 카운팅 변수
     
-    for (let x=0; x<h; x++) {
-        for (let y=0; y<w; y++) {
-            if (maps[x][y] && !visited[x][y]) {
-                bfs([x, y], maps, w, h, visited);
+    for (let x = 0; x < h; x++) {
+        for (let y = 0; y < w; y++) {
+            if (!visited[x][y] && maps[x][y]) {
+                dfs([x, y], w, h, visited, maps);
                 count++;
             }
         }
     }
-    console.log(count);
+    result.push(count);
 }
-                
-     
+
+console.log(result.join('\n'));
