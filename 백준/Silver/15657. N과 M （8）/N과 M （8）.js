57895dcf06b9e84570ce65cdfdcb0549144947b4
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-12 14:42:20 +0900

    [Silver III] Title: N과 M (8), Time: 436 ms, Memory: 13356 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, m] = input.shift().split(" ").map(Number);
+const arr = input.shift().split(' ').map(Number).sort((a,b) => a-b);
+const seq = Array(m).fill(0);
+let ans = '';
+
+const dfs = (dep, start) => {
+    if (dep === m) {
+        console.log(seq.join(' '));
+        return;
+    }
+    
+    for (let i=start; i<n; i++) {
+        seq[dep] = arr[i];
+        dfs(dep+1, i);
+    }
+}
+
+dfs(0, 0);
