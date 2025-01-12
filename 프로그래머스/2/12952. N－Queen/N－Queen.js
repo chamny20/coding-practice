0d532f273edf22b9ad3177ead295991f1d8ed3c5
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-12 21:53:34 +0900

    [level 2] Title: N-Queen, Time: 114.40 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,27 +1,4 @@
 function solution(n) {
     var answer = 0;
-    const visited1 = Array(n).fill(false);
-    const visited2 = Array(n).fill(false);
-    const visited3 = Array(n).fill(false);
-    
-    const dfs = (dep) => {
-        if (dep === n) {
-            answer++;
-            return;
-        }
-        
-        for (let i=0; i<n; i++) {
-            if (visited1[i] || visited2[dep+i] || visited3[dep-i+n-1]) {
-                continue;
-            } 
-            visited1[i] = visited2[dep+i] = visited3[dep-i+n-1] = true;
-            dfs(dep + 1);
-            visited1[i] = visited2[dep+i] = visited3[dep-i+n-1] = false;
-            
-        }
-    }
-    
-    dfs(0);
-    
     return answer;
 }
