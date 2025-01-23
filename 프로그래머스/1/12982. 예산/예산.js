5895560f345ba41fe3cccdfc106b801e468dd6d1
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-23 10:55:37 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,14 +1,15 @@
 function solution(d, budget) {
-    var answer = 0;
+    d.sort((a, b) => a-b);
     let sum = 0;
-    d.sort((a,b) => a-b);
+    let i = 0;
+    const ans = [];
     
-    for(let i=0; i<d.length; i++) {
-        answer++;
-        sum += d[i];
+    for (let i=0; i<d.length; i++) {
+        if (sum + d[i] > budget) break;
         
-        if(sum>budget)
-            answer--;
+        sum += d[i];
+        ans.push(d[i]);
     }
-    return answer;
+    return ans.length;
+    
 }
