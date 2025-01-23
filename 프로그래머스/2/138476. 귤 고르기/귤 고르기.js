36569b42b304cd12e7ac34955e9b6409d02d10a1
Chaemin Lee <dge3179@ajou.ac.kr>
2025-01-23 12:09:40 +0900

    [level 2] Title: 귤 고르기, Time: 148.06 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,35 +1,26 @@
 function solution(k, tangerine) {
-    // tangerine.sort((a,b) => a-b);
-    let count = 0;
-    let result = 0;
-    let tan = {};
+    let ans = 0;
+    let obj = {};
     
-    tangerine.forEach((idx) => {
-        //key가 idx
-        tan[idx] = ++tan[idx] || 1;
-    })
-
-    let arr = Object.values(tan).sort((a,b) => b-a);
+    tangerine.forEach((num) => {
+        if (!obj[num]) obj[num] = 0;
+        obj[num]++;
+    });
+    obj = Object.entries(obj).sort((a, b) => b[1] - a[1]);
     
-    for(let j of arr) {
-        if (count < k) {
-            count += j;
-            result++;
+    while (k > 0) {
+        for (let [num, cnt] of obj) {
+            if (k <= cnt) {
+                ans++;
+                k -= cnt;
+                return ans;
+            }
+            if (k === 0) break;
+            k -= cnt;
+            ans++;
         }
-        continue;
     }
     
-   
-    return result;
+    return ans;
 }
 
-/*
-
-k개 고르기 => 상자 하나에 담아 판매
-서로 다른 종류의 수 최소화
-
-[1,3,2,5,4,5,2,3] => 1 : 1개 / 2 : 2개 / 3 : 2개 / 4 : 1개 / 5 : 2개
-
-크기 개수를 담은 배열을 만들자!
-
-*/
