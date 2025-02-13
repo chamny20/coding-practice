48e03e2d89e1a73e0268bef971bb014eba6cc315
Chaemin Lee <dge3179@ajou.ac.kr>
2025-02-13 21:36:45 +0900

    [Gold V] Title: LCS, Time: 204 ms, Memory: 24780 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+const str1 = input[0];
+const str2 = input[1];
+
+const n = str1.length;
+const m = str2.length;
+const dp = Array.from(Array(n+1), () => Array(m+1).fill(0));
+
+for (let i=1; i<=n; i++) {
+    for (let j=1; j<=m; j++) {
+        if (str1[i-1] === str2[j-1])
+            dp[i][j] = dp[i-1][j-1] + 1;
+        else 
+            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+    }
+}
+
+console.log(dp[n][m]);
