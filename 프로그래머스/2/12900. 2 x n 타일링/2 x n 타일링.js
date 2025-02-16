b5e7a02b33628272b631fb3027ba89495fcd6818
Chaemin Lee <dge3179@ajou.ac.kr>
2025-02-16 10:06:36 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.38 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,17 +1,11 @@
 function solution(n) {
-    var answer = 0;
-    const dp = new Array(n).fill(0);
-    
-    if (n === 1) return 1;
-    if (n === 2) return 2;
-    
+    const dp = Array(n+1).fill(0);
     dp[1] = 1;
     dp[2] = 2;
     
     for (let i=3; i<=n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
     }
     
-    answer = dp[dp.length - 1];
-    return answer;
+    return dp[n];
 }
