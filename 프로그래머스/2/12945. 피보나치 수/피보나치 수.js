bdb69ca283ca5f0e25e86bc7cc1935d14006fb0d
Chaemin Lee <dge3179@ajou.ac.kr>
2025-02-16 09:55:22 +0900

    [level 2] Title: 피보나치 수, Time: 258.67 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,24 +1,9 @@
 function solution(n) {
-    if(n==0)
-        return 0;
-    if(n==1||n==2)
-        return 1;
+    const fib = [0, 1]; // fib[0] = 0, fib[1] = 1
     
-    let fibo = [0,1,1];
-    
-    for(let i=3; i<=n; i++) {
-        fibo[i] = (fibo[i-1] + fibo[i-2])%1234567; 
+    for (let i=2; i<=n; i++) {
+        fib.push((fib[i-1] + fib[i-2]) % 1234567);
     }
-    
-    
-    return fibo[n];
+    return fib[n];
 }
 
-
-/*
-first try :런타임 시간 초과 문제 발생
-=> 중복호출이 많을 경우 런타임이 길어진다!
-
-
-
-*/
