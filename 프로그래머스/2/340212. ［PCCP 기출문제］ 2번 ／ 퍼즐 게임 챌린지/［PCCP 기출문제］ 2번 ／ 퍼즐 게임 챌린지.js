6c2cb9cf5e28843fd77c93bac82232e5d5e6fe12
Chaemin Lee <dge3179@ajou.ac.kr>
2025-03-02 14:20:10 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 40.61 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,30 +1,4 @@
 function solution(diffs, times, limit) {
-    let min = 1;
-    let max = 100000;
-    times.unshift(0);
-    
-    while (min <= max) {
-        let sum = 0;
-        let level = Math.floor((min + max) / 2);
-        
-        for (let i=0; i<diffs.length; i++) {
-            const diff = diffs[i];
-            // console.log('diff', diff);
-            if (diff > level) {
-                sum += ((diff - level) * (times[i] + times[i+1]) + times[i+1]);
-            } else {
-                sum += times[i+1];
-            }
-        }
-
-        if (sum > limit) {
-            min = level + 1;
-        } else {
-            max = level - 1;
-        }
-        // console.log(sum)
-        // console.log(min, max)
-    }
-
-    return min;
+    var answer = 0;
+    return answer;
 }
