b79062497602d5c33f473e17d3978735de90c28f
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-02 10:39:49 +0900

    [level 2] Title: 전화번호 목록, Time: 154.85 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,15 +1,11 @@
 function solution(phone_book) {
-    var answer = true;
     phone_book.sort();
     console.log(phone_book);
     
-    for(let i=0; i<phone_book.length-1; i++) {
-        if(phone_book[i] == phone_book[i+1].substring(0,phone_book[i].length)) {
-            answer = false;
-            break;
-            console.log(phone_book[i]);
+    for (let i=0; i<phone_book.length - 1 ; i++) {
+        if (phone_book[i] === phone_book[i+1].substring(0, phone_book[i].length)) {
+            return false;
         }
     }
-    
-    return answer;
+    return true;
 }
