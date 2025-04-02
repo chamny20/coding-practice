88c5f552a536e02440792c79e65b6084285a4f40
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-02 12:16:10 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,28 +1,18 @@
 function solution(clothes) {
-    var answer = 1;
-    let arr = {};
+    const obj = {};
+    const n = clothes.length;
     
-    clothes.forEach((item) => {
-        console.log("item:", item);
-        if (!arr[item[1]])
-            arr[item[1]] = 0;
-        
-        arr[item[1]] += 1;
-    })
-    // console.log(arr);
-
-    for (let key in arr) {
-        // console.log(arr[key]);
-        //A,B,C일 경우 => (A+1)(B+1)(C+1)
-        answer *= arr[key] + 1;
+    for (let i=0; i<n; i++) {
+        const type = clothes[i][1];
+        obj[type] = obj[type] || [];
+        obj[type].push(clothes[i][0]);
     }
     
-    return answer-1;
+    let ans = 1;
+    // (A+1)(B+1)(C+1) = ABC+AB+AC+BC+A+B+C+1
+    for (const key in obj) {
+        ans *= obj[key].length + 1;
+    }
+    
+    return ans - 1;
 }
-
-/*
-
-종류별 1가지 의상만 착용 가능
-해시 객체 이용해서... 조합 맞추기
-
-*/
