e7fd2247fd987b7d296c7fe1d05921a592225699
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-05 23:46:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 407.42 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,35 +1,34 @@
 function solution(id_list, report, k) {
-    let answer = [];
-    let reportedCnt = {};
-    let reporterObj = {};
+    // 신고한 목록 확인 가능한 obj
+    const perObj = {};
+    // report 당한 횟수
+    const reportCnt = {};
+    
+    id_list.forEach((name) => {
+        perObj[name] = perObj[name] || [];
+        reportCnt[name] = reportCnt[name] || 0;
+    });
+    
+    report.forEach((list) => {
+        const [from, to] = list.split(' ');
+        
+        if (!perObj[from].includes(to)) {
+            perObj[from].push(to);
+            reportCnt[to] += 1;
+        }
+    })
 
-    for (let id of id_list) {
-        reporterObj[id] = new Set(); 
-    }
-    // console.log('here', reporterObj)
+    // console.log(perObj);
+    // console.log(reportCnt);
+    // filtering reportCnt
+    const people = Object.keys(reportCnt).filter(n => reportCnt[n] >= k);
     
-    for (let i=0; i<report.length; i++) {
-        const [a, b] = report[i].split(" ");
-        if(!reportedCnt[b]) reportedCnt[b] = 0;
-        
-        if(!reporterObj[a].has(b)) {
-            reporterObj[a].add(b);
-            reportedCnt[b] += 1;
-        }
-        
+    const ans = [];
+    for (const key in perObj) {
+        let cnt = 0;
+        const arr = perObj[key];
+        cnt += arr.filter(name => people.includes(name)).length;
+        ans.push(cnt);
     }
-    // console.log(reporterObj)
-    
-    for (let id of id_list) {
-        let count = 0;
-        // console.log('여기', reporterObj[id])
-        for (let reported of reporterObj[id]) {
-            if (reportedCnt[reported] >= k) {
-                count++;
-            }
-        }
-        answer.push(count);
-    }
-    
-    return answer;
+    return ans;
 }
