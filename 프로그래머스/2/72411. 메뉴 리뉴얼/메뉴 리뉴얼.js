7217eee4780786918757cb137eea34488c070904
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-07 16:54:49 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 45.03 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,44 +1,47 @@
 function solution(orders, course) {
-    var answer = [];
-    
-    function combinations(arr, n) {
-        if (n === 1) return arr.map((item) => [item]);
+    // 조합 함수
+    const getCombination = (arr, n) => {
         const result = [];
+        if (n === 1) return arr.map(v => [v]);
         
-        arr.forEach((fixed, idx, arr) => {
-            const rest = arr.slice(idx+1);
-            const combis = combinations(rest, n-1);
-            const combine = combis.map((v) => [fixed, ...v]);
+        arr.forEach((select, idx) => {
+            const rest = arr.slice(idx + 1);
+            const combis = getCombination(rest, n-1);
+            const combine = combis.map((v) => [select, ...v]);
             result.push(...combine);
         })
         return result;
-    }
+    };
     
-    for (let c of course) {
+    const ans = [];
+   
+    for (let i=0; i<course.length; i++) {
+        const n = course[i];
         const menu = [];
-        for (let i=0; i<orders.length; i++) {
-            let arr = orders[i].split("").sort();
-            let cmb = combinations(arr, c);
-            menu.push(...cmb);
-        }
-        // console.log(menu);
+        
+        orders.forEach(item => {
+            let arr = getCombination(item.split('').sort(), n);
+            menu.push(...arr);
+        });
         const counter = {};
         
+        // console.log(menu);
         for (const m of menu) {
-            let key = m.join("");
+            let key = m.join('');
             counter[key] = (counter[key] || 0) + 1;
-            // console.log(counter);
-            
         }
+        // console.log(counter);
         const max = Math.max(...Object.values(counter));
-            if (max > 1) {
-                for (const [key, value] of Object.entries(counter)) {
-                    if (value === max)
-                        answer.push(key);
-                }
-            }
         
+        if (max > 1) {
+            for (const key in counter) {
+                if (counter[key] === max)
+                    ans.push(key);
+            }
+        }
+        // console.log(ans)
     }
     
-    return answer.sort();
+  
+    return ans.sort();
 }
