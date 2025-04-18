895806c12d35e6dd6013f27418a6803099b2e8bc
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-19 00:54:13 +0900

    [level 2] Title: 모음 사전, Time: 4.94 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,20 +1,20 @@
 function solution(word) {
-    const vowels = ['A', 'E', 'I', 'O', 'U'];
-    // 모든 경우의 수 => 5**5  + 5**4 + 5**3 + 5**2 + 5
+    const list = ['A', 'E', 'I', 'O', 'U'];
+    // case => 5**5 + 5**4 + 5**3 + 5**2 + 5
+    
     const arr = [];
-
-    const dfs = (cur, len) => {
+    
+    const dfs = (str, len) => {
         if (len > 5) {
             return;
         }
-        arr.push(cur);
         
-        for (const str of vowels) {
-            dfs(cur + str, len + 1);
+        arr.push(str);
+        
+        for (const s of list) {
+            dfs(str + s, len + 1);
         }
     }
     dfs('', 0);
-    console.log(arr)
-    
     return arr.indexOf(word);
 }
