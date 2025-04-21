cb6e32c8a5a34c2753790e632f847a8339b17c2d
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-21 13:58:49 +0900

    [level 1] Title: 모의고사, Time: 6.12 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 1.74 ms
+메모리: 36.9 MB, 시간: 6.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 11:27:14
+2025년 04월 21일 13:58:46
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,19 +1,30 @@
 function solution(answers) {
-    const p1 = [1, 2, 3, 4, 5];
-    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const p1 = [1, 2, 3, 4, 5]; // 5
+    const p2 = [2, 1, 2, 3, 2, 4, 2, 5]; // 8
+    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]; //10
     
-    const result = [];
+    const obj = { '1': 0, '2': 0, '3': 0};
+    const n = answers.length;
     
-    let p1Len = answers.filter((num, i) => num === p1[i%p1.length]).length;
-    let p2Len = answers.filter((num, i) => num === p2[i%p2.length]).length;
-    let p3Len = answers.filter((num, i) => num === p3[i%p3.length]).length;
+    for (let i=0; i<n; i++) {
+        const num = answers[i];
+        if (num === p1[i%5]) {
+            obj['1']++;
+        }
+        if (num === p2[i%8]) {
+            obj['2']++;
+        }
+        if (num === p3[i%10]) {
+            obj['3']++;
+        }
+        
+    }
+    const ans = [];
     
-    let max = Math.max(p1Len, p2Len, p3Len);
-    if (max === p1Len) result.push(1);
-    if (max === p2Len) result.push(2);
-    if (max === p3Len) result.push(3);
-    
-    return result;
-    
+    const max = Math.max(...Object.values(obj));
+    for (const key in obj) {
+        if (max === obj[key]) ans.push(Number(key));
+    }
+     
+    return ans;
 }
