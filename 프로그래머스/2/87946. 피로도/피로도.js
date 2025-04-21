4d911c9693fecf8bc7eac3d71b0ffb26188410e1
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-21 17:14:07 +0900

    [level 2] Title: 피로도, Time: 8.67 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,32 +1,23 @@
 function solution(k, dungeons) {
-    let answer = 0;
-    const len = dungeons.length;
-    const visited = Array(len).fill(false);
+    const n = dungeons.length;
+    const visited = Array(n).fill(false);
+    let ans = 0;
     
     const dfs = (cur, cnt) => {
-        answer = Math.max(answer, cnt);
+        ans = Math.max(ans, cnt);
         
-        for (let i=0; i<len; i++) {
-            const [needs, consumption] = dungeons[i];
-            
-            if (!visited[i] && cur >= needs) {
+        for (let i=0; i<n; i++) {
+            const [a, b] = dungeons[i];
+
+            if (!visited[i] && cur >= a) {
                 visited[i] = true;
-                dfs(cur-consumption, cnt+1);
+                dfs(cur - b, cnt + 1);
                 visited[i] = false;
             }
         }
-        
     }
+
     dfs(k, 0);
- 
-    return answer;
+    
+    return ans;
 }
-
-/* 
-하루에 한번 탐험 가능
-최대한 많이 탐험해야 함
-k: 유저 현재 피로도
-dungenos
-최소 필요 피로도, 소모 피로도
-
-*/
