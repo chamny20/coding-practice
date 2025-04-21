f2242ddc6702543d71b2202b2c431917109e9a70
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-21 23:19:45 +0900

    [level 2] Title: 가장 큰 수, Time: 165.24 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,11 +1,11 @@
 function solution(numbers) {
-    const strArr = numbers.map(el => el.toString()).sort((a, b) => {
-        const num1 = a + b;
-        const num2 = b + a;
-        return num1 > num2 ? -1 : 1;
-    });
+    // const strArr = numbers.map(el => el.toString()).sort((a, b) => {
+    //     const num1 = a + b;
+    //     const num2 = b + a;
+    //     return num1 > num2 ? -1 : 1;
+    // });
+    const strArr = numbers.map(String).sort((a, b) => (b + a) - (a + b));
     
-    if (strArr[0] === '0') return '0';
-    return strArr.join('');
+    return Number(strArr.join('')) === 0 ? '0' : strArr.join('');
     
 }
