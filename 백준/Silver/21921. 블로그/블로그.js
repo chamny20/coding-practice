aeaef7475fa3551bf723a2472661d9a319447bf9
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-24 15:42:47 +0900

    [Silver III] Title: 블로그, Time: 232 ms, Memory: 34248 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.js
+++ 백준/Silver/21921. 블로그/블로그.js
@@ -2,22 +2,32 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n'
 const [n, x] = input.shift().split(' ').map(Number);
 const arr = input.shift().split(' ').map(Number);
 
-for (let i=1; i<n; i++) {
-    arr[i] += arr[i-1];
+/** 슬라이딩 윈도우 로직
+* 1) 처음 x일의 합 먼저 구하기
+* 2) 맨 앞요소 빼고, 새 요소 더하면서 합 유지
+* 3) 최댓값과 그 횟수 계산
+*/
+let sum = 0;
+for (let i=0; i<x; i++) {
+    sum += arr[i];
 }
 
-const result = [arr[x-1]];
+let max = sum;
+let cnt = 1; // 횟수 카운팅
 
-for (let i=x; i<n; i++) {
-    let sum = arr[i] - arr[i-x];
-    result.push(sum);
+for (let i=x; i<n; i++) { 
+    sum = sum - arr[i-x] + arr[i]; // 슬라이딩 윈도우 (이전꺼 하나 제거, 새거 하나 추가)
+    
+    if (sum > max) {
+        max = sum;
+        cnt = 1;
+    } else if (sum === max) {
+        cnt++;
+    }
 }
-
-let max = Math.max(...result);
-const cnt = result.filter(el => el === max).length;
 
 if (max === 0) {
-    console.log("SAD");
+    console.log('SAD');
 } else {
     console.log(max);
     console.log(cnt);
