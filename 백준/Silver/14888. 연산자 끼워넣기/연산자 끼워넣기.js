77ac912f63ef5b38ef9cc56bae4352c50b4e626b
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-24 22:33:38 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 11216 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,38 +1,43 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 const n = Number(input.shift());
 const arr = input.shift().split(' ').map(Number);
 const operator = input.shift().split(' ').map(Number);
 
-let maxVal = -Infinity;
-let minVal = Infinity;
+let max = -Infinity;
+let min = Infinity;
 
-const divide = (a, b) => {
-    if (a < 0)
-        return -Math.floor(-a / b);
-    return Math.floor(a / b);
+const divide = (num1, num2) => {
+    if (num1 < 0)
+        return -Math.floor(-num1 / num2);
+    return Math.floor(num1 / num2);
 }
 
+
 const dfs = (dep, value) => {
-    if (dep === n) { // 모든 숫자를 다 사용했을 경우
-        maxVal = Math.max(maxVal, value);
-        minVal = Math.min(minVal, value);
+    if (dep === n) {
+        min = Math.min(min, value);
+        max = Math.max(max, value);
         return;
     }
     
     for (let i=0; i<4; i++) {
         if (operator[i] > 0) {
-            operator[i] -= 1;
+            operator[i]--;
             
-            if (i===0) dfs(dep+1, value+arr[dep]);
-            else if (i===1) dfs(dep+1, value-arr[dep]);
-            else if (i===2) dfs(dep+1, value*arr[dep]);
-            else if (i===3) dfs(dep+1, divide(value, arr[dep]));
+            if (i === 0) dfs(dep+1, value + arr[dep]);
+            else if (i === 1) dfs(dep+1, value - arr[dep]);
+            else if (i === 2) dfs(dep+1, value * arr[dep]);
+            else 
+                dfs(dep+1, divide(value, arr[dep]));
             
-            operator[i] += 1;
+            // back
+            operator[i]++;
         }
     }
 }
 
+
 dfs(1, arr[0]);
-console.log(maxVal ? maxVal : 0);
-console.log(minVal ? minVal : 0);
+console.log(max ? max : 0);
+console.log(min ? min : 0);
+
