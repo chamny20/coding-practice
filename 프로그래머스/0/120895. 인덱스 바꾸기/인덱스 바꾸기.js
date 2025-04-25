0d2c5dc057714ff9257c0b3e890d5fe66885ac3e
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-25 13:45:48 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,17 +1,5 @@
 function solution(my_string, num1, num2) {
-    const n1 = my_string[num1];
-    const n2 = my_string[num2];
-    let str = '';
-    
-    for (let i=0; i<my_string.length; i++) {
-        if (i === num1)
-            str += n2;
-        else if (i === num2)
-            str += n1;
-        else
-            str += my_string[i];
-            
-    }
-    
-    return str;
+    let arr = my_string.split('');
+    [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+    return arr.join('');
 }
