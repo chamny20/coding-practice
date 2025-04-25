3abad1e2256c4ab2a486fd41867557c42c4da96b
Chaemin Lee <dge3179@ajou.ac.kr>
2025-04-25 12:01:01 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, letter) {
-    let str = '';
-    
-    my_string.split('').forEach(st => {
-        if (st !== letter)
-            str += st;
-    })
-    
-    return str;
+    return my_string.split(letter).join('');
 }
