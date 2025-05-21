12ad7ed75a3ab4136b33e169c706d46b2a918ac3
Chaemin Lee <dge3179@ajou.ac.kr>
2025-05-21 20:42:24 +0900

    [level 2] Title: 카펫, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,18 +1,17 @@
 function solution(brown, yellow) {
+    // b + y = 12
+    const sum = brown + yellow;
     let ans = [0, 0];
-    const area = brown + yellow;
     
-    for (let h=3; h<=Math.sqrt(area); h++) {
-        if (area % h === 0) {
-            const w = area / h;
-            
+    for (let h=3; h<=Math.sqrt(sum); h++) {
+        if (sum % h === 0) {
+            const w = sum / h;
             const innerW = w - 2;
             const innerH = h - 2;
             
-            if (innerW * innerH === yellow) {
+            if (innerW * innerH === yellow) 
                 ans = [w, h];
-            }
-        }
+        } 
     }
     
     return ans;
