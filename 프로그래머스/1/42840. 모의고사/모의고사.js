11400373078dc6266cfd18bbc36e6d34feae47c2
Chaemin Lee <dge3179@ajou.ac.kr>
2025-05-21 20:22:42 +0900

    [level 1] Title: 모의고사, Time: 1.75 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,30 +1,18 @@
 function solution(answers) {
-    const p1 = [1, 2, 3, 4, 5]; // 5
-    const p2 = [2, 1, 2, 3, 2, 4, 2, 5]; // 8
-    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]; //10
+    let p1 = [1, 2, 3, 4, 5];
+    let p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    const obj = { '1': 0, '2': 0, '3': 0};
-    const n = answers.length;
+    const p1Len = answers.filter((answer, i) => answer === p1[i%p1.length]).length;
+    const p2Len = answers.filter((answer, i) => answer === p2[i%p2.length]).length;
+    const p3Len = answers.filter((answer, i) => answer === p3[i%p3.length]).length;
+
+    const result = [];
+    let max = Math.max(p1Len, p2Len, p3Len);
     
-    for (let i=0; i<n; i++) {
-        const num = answers[i];
-        if (num === p1[i%5]) {
-            obj['1']++;
-        }
-        if (num === p2[i%8]) {
-            obj['2']++;
-        }
-        if (num === p3[i%10]) {
-            obj['3']++;
-        }
-        
-    }
-    const ans = [];
+    if (max === p1Len) result.push(1);
+    if (max === p2Len) result.push(2);
+    if (max === p3Len) result.push(3);
     
-    const max = Math.max(...Object.values(obj));
-    for (const key in obj) {
-        if (max === obj[key]) ans.push(Number(key));
-    }
-     
-    return ans;
+    return result;
 }
