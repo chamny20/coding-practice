2cafe4266180de064ea85461b5a41020a9c090a6
Chaemin Lee <dge3179@ajou.ac.kr>
2025-05-21 20:31:27 +0900

    [level 2] Title: 소수 찾기, Time: 5.44 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,34 +1,34 @@
 function solution(numbers) {
     const arr = numbers.split('').map(Number);
-    const ans = [];
     
     const isPrime = (num) => {
         if (num <= 1) return false;
         if (num === 2) return true;
-        
         for (let i=2; i<=Math.sqrt(num); i++) {
-            if (num % i === 0)
-                return false;
+            if (num % i === 0) return false;
         }
         return true;
     }
     
-    const getPermutation = (arr, fixed) => {
+    const ans = [];
+    
+    const dfs = (arr, fixed) => {
         if (arr.length >= 1) {
             for (let i=0; i<arr.length; i++) {
                 const newNum = fixed + arr[i];
-                const copyArr = [...arr];
-                copyArr.splice(i, 1);
+                const copy = [...arr];
+                copy.splice(i, 1);
                 
                 if (!ans.includes(+newNum) && isPrime(+newNum)) {
                     ans.push(+newNum);
                 }
-                getPermutation(copyArr, newNum);
+                
+                dfs(copy, newNum);
             }
         }
     }
     
-    getPermutation(arr, '');
+    dfs(arr, '');
     
     return ans.length;
 }
