2ac7cd275bf428b2c7260d3dffcf71854a7b273a
Chaemin Lee <dge3179@ajou.ac.kr>
2025-06-12 00:03:37 +0900

    [level 2] Title: 큰 수 만들기, Time: 56.12 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -2,14 +2,13 @@ function solution(number, k) {
     const stack = [];
     
     for (let i=0; i<number.length; i++) {
-        const str = Number(number[i]);
-        
-        while (number.length > 0 && k > 0 && stack.at(-1) < str) {
+        const num = Number(number[i]);
+
+        while (stack.length > 0 && k > 0 && stack.at(-1) < num) {
             stack.pop();
             k--;
         }
-        stack.push(str);
+        stack.push(num);
     }
-    
     return stack.slice(0, number.length - k).join('');
 }
