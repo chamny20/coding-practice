1093391797d256df23fb3273a16cc3a74be25b32
Chaemin Lee <dge3179@ajou.ac.kr>
2025-11-04 20:31:34 +0900

    [Silver III] Title: 두 수의 합, Time: 184 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -1,23 +1,25 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input.shift());
 const arr = input.shift().split(' ').map(Number);
 const x = Number(input.shift());
 
 arr.sort((a, b) => a - b);
+// [1, 2, 3, 5, 7, 9, 10, 11, 12]
+
 let left = 0;
 let right = n-1;
-
 let cnt = 0;
+const result = [];
+
 while (left < right) {
     let sum = arr[left] + arr[right];
-    
     if (sum === x) {
         cnt++;
-        left++;
         right--;
+        left++;
     } else if (sum < x) {
         left++;
-    } else {
+    } else if (sum > x) {
         right--;
     }
 }
