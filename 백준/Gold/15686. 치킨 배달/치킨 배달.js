bd3737c9ea40819ff18465b8208abb41d436761a
Chaemin Lee <dge3179@ajou.ac.kr>
2025-12-09 23:19:15 +0900

    [Gold V] Title: 치킨 배달, Time: 200 ms, Memory: 12736 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -1,44 +1,45 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
 const [n, m] = input.shift().split(' ').map(Number);
-// 폐업시키지 않을 치킨집은 m개
-const maps = input.map((line) => line.split(' ').map(Number));
-const homeArr = [];
-const chickenArr = [];
+// 최대 m개 골라야 함
+// 반환값은 도시의 치킨 거리의 최솟값
+const houses = [];
+const chickens = [];
 let ans = Infinity;
 
-for (let x=0; x<n; x++) {
-    for (let y=0; y<n; y++) {
-        if (maps[x][y] === 1) {
-            homeArr.push([x, y]);
-        } else if (maps[x][y] === 2) {
-            chickenArr.push([x, y]);
+for (let i=0; i<n; i++) {
+    const line = input[i].split(' ').map(Number);
+    for (let j=0; j<n; j++) {
+        if (line[j] === 1) {
+            houses.push([i+1, j+1]);
+        } else if (line[j] === 2) {
+            chickens.push([i+1, j+1]);
         }
     }
 }
 
-let visited = Array(chickenArr.length).fill(false);
+
+const visited = Array(chickens.length).fill(false);
 
 const dfs = (idx, cnt) => {
     if (cnt === m) {
         let totalDistance = 0;
-        for (const [hx, hy] of homeArr) {
-            let minDistance = Infinity;
-            
-            for (let i=0; i<chickenArr.length; i++) {
+        for (const [hx, hy] of houses) {
+            let minDis = Infinity;
+            for (let i=0; i<chickens.length; i++) {
+                const [cx, cy] = chickens[i];
                 if (visited[i]) {
-                    const [cx, cy] = chickenArr[i];
-                    const dis = Math.abs(cx-hx) + Math.abs(cy-hy);
-                    
-                    minDistance = Math.min(dis, minDistance);
+                    const dis = Math.abs(hx-cx) + Math.abs(hy-cy);
+                    minDis = Math.min(minDis, dis);
                 }
             }
-            totalDistance += minDistance;
+            totalDistance += minDis;
+
         }
         ans = Math.min(ans, totalDistance);
         return;
     }
     
-    for (let i=idx; i<chickenArr.length; i++) {
+    for (let i=idx; i<chickens.length; i++) {
         if (!visited[i]) {
             visited[i] = true;
             dfs(i+1, cnt+1);
@@ -48,4 +49,5 @@ const dfs = (idx, cnt) => {
 }
 
 dfs(0, 0);
+
 console.log(ans);
