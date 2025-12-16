24ed85c62a9a80f262f5e2f6854ea0adbc7ec689
Chaemin Lee <dge3179@ajou.ac.kr>
2025-12-17 00:20:37 +0900

    [Gold V] Title: 암호 만들기, Time: 148 ms, Memory: 11208 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.js
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.js
@@ -1,34 +1,28 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [l, c] = input.shift().split(' ').map(Number);
-const arr = input[0].split(' ').sort();
-const vowels = ['a', 'e', 'i', 'o', 'u']; // 모음
-const pwd = [];
+const [L, C] = input.shift().split(' ').map(Number);
+const originArr = input.shift().split(' ');
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+originArr.sort();
 const ans = [];
-const visited = Array(c).fill(false);
 
 const dfs = (idx, str) => {
- 
-    if (str.length === l) {
+    if (str.length === L) {
+        // 모음 1개 이상 && 자음 2개 이상이어야 통과
         let vowCnt = 0;
         let conCnt = 0;
         for (let i=0; i<str.length; i++) {
-            if (str.includes(vowels[i])) {
-                vowCnt++;
-            } else 
-                conCnt++;
+            if (vowels.includes(str[i])) vowCnt++;
+            else conCnt++;
         }
-        if (vowCnt >=1 && conCnt >=2) {
+        if (vowCnt >= 1 && conCnt >= 2) {
             ans.push(str);
+            return;
         }
-        return;
     }
     
-    for (let i=idx; i<arr.length; i++) {
-        if (!visited[i]) {
-            visited[i] = true;
-            dfs(i+1, str+arr[i]);
-            visited[i] = false;
-        }
+    for (let i=idx; i<C; i++) {
+        dfs(i+1, str + originArr[i]);
     }
 }
 
