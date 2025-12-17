b53fecaad86ec7cd64e9b8997ff5801a8e1e558b
Chaemin Lee <dge3179@ajou.ac.kr>
2025-12-17 23:36:50 +0900

    [Silver III] Title: N과 M (1), Time: 1608 ms, Memory: 13812 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,28 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, m] = input.shift().split(' ').map(Number);
+// 1 ~ n -> 중복없이 m개 고른 수열
+const visited = Array(n).fill(false);
+const arr = [];
+
+const dfs = (idx) => {
+    if (idx === m) {
+        console.log(arr.join(' '));
+        return;
+    }
+    
+    
+    for (let i=0; i<n; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            
+            arr.push(i+1);
+            dfs(idx+1);
+            arr.pop();
+            
+            visited[i] = false;
+        }
+    }
+}
+
+
+dfs(0);
