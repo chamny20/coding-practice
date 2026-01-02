1ed017d36afac119563f61135255fdb12d4e377d
Chaemin Lee <dge3179@ajou.ac.kr>
2026-01-02 16:05:32 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 9400 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.js
@@ -0,0 +1,36 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const n = Number(input.shift());
+// nxn
+const maps = [];
+const dp = Array.from(Array(n), () => Array.from(Array(n), () => Array(3).fill(0)));
+dp[0][1][0] = 1;
+
+// 방향 d => 0: 가로, 1: 세로, 2: 대각선
+
+for (let i=0; i<n; i++) {
+    const line = input[i].split(' ').map(Number);
+    maps.push(line);
+}
+
+for (let i=1; i<n; i++) {
+    for (let j=2; j<n; j++) {
+        if (!maps[0][j]) {
+            dp[0][j][0] = dp[0][j-1][0];
+        }
+        
+        // 가로
+        if (!maps[i][j]) {
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+        }
+        // 세로
+        if (!maps[i][j]) {
+            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+        }
+        // 대각선
+        if (!maps[i][j] && !maps[i][j-1] && !maps[i-1][j])
+            dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+    } 
+}
+
+const ans = dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
+console.log(ans);
