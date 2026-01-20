d5fb203b54a2938d208645585309195dd32e928a
Chaemin Lee <dge3179@ajou.ac.kr>
2026-01-20 18:10:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 9532 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,33 +1,43 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input.shift());
-const dir = [[1, 0], [-1, 0], [0, -1], [0, 1]];
-const maps = input.map((v) => v.split("").map(Number));
-const visited = Array.from(Array(n), () => Array(n).fill(false));
-let result = [];
+const maps = input.map(line => line.split('').map(Number));
+const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-const dfs = (start, visited) => {
-    let cnt = 1; // 집 개수
-    visited[start[0]][start[1]] = true;
+const bfs = (start, visited) => {
+    const [x, y] = start;
+    visited[x][y] = true;
+    const queue = [[x, y]];
+    let dis = 1;
     
-    for (let i=0; i<dir.length; i++) {
-        const nx = start[0] + dir[i][0];
-        const ny = start[1] + dir[i][1];
+    while (queue.length) {
+        const [curX, curY] = queue.shift();
         
-        if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && maps[nx][ny]) {
-            cnt += dfs([nx, ny], visited); // 연결된 집을 재귀적으로 탐색하여 개수 누적
+        for (let i=0; i<dirs.length; i++) {
+            const nx = curX + dirs[i][0];
+            const ny = curY + dirs[i][1];
+            if (nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] && maps[nx][ny]) {
+                visited[nx][ny] = true;
+                queue.push([nx, ny]);
+                dis++;
+            }
         }
+        
     }
-    return cnt;
+    
+    return dis;
 }
 
-for (let x=0; x<n; x++) {
-    for (let y=0; y<n; y++) {
-        if (!visited[x][y] && maps[x][y]===1) {
-            let cnt = dfs([x, y], visited);
-            result.push(cnt);
+const res = [];
+const visited = Array.from(Array(n), () => Array(n).fill(false));
+
+for (let i=0; i<n; i++) {
+    for (let j=0; j<n; j++) {
+        if (!visited[i][j] && maps[i][j]) {
+            const dis = bfs([i, j], visited);
+            res.push(dis);
         }
     }
 }
 
-console.log(result.length);
-console.log(result.sort((a,b) => a-b).join("\n"));
+console.log(res.length);
+console.log(res.sort((a, b) => a - b).join('\n'));
