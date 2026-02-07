1caaac7a08da75f670a448eaf2ac5aacab6dc59f
Chaemin Lee <dge3179@ajou.ac.kr>
2026-02-07 19:35:20 +0900

    [Bronze I] Title: 단어 공부, Time: 188 ms, Memory: 13056 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -1,22 +1,24 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().toUpperCase();
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const s = input[0].toLowerCase();
 
-let strCount = {};
-
-for (let char of input) {
-    if (strCount[char]) {
-        strCount[char]++;
-    } else {
-        strCount[char] = 1;
-    }
+const map = new Map();
+for (let i=97; i<=122; i++) {
+    map.set(i, 0);
 }
 
-let strCountArr = Object.entries(strCount);
-strCountArr.sort((a, b) => b[1] - a[1]);
-
-let maxKey = strCountArr[0][0];
-if (strCountArr.length > 1 && strCountArr[0][1] === strCountArr[1][1]) {
-    console.log("?");
-} else {
-    console.log(maxKey);
+for (let i=0; i<s.length; i++) {
+    const num = s[i].charCodeAt();
+    map.set(num, map.get(num) + 1);
 }
 
+
+const arr = [...map.entries()];
+let ansList = arr.sort((a, b) => b[1] - a[1]);
+const maxVal = arr[0][1]; 
+ansList = ansList.filter(([k, v]) => maxVal === v);
+
+if (ansList.length > 1) console.log('?');
+else {
+    const [k, v] = ansList[0];
+    console.log(String.fromCharCode(k).toUpperCase());
+}
