aee426ae4c5e143e923ec6a7ec756b4a67b2cd2a
Chaemin Lee <dge3179@ajou.ac.kr>
2026-02-07 20:02:26 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 88 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,9 +1,13 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+let str = input[0];
 
-let alp = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+// 일단 크로아티아 몇갠지 계산필요
+// 남은 알파벳 카운트해서 더하면 됨
 
-for (let i of alp) {
-    input = input.replaceAll(i, '.');
+for (let i=0; i<list.length; i++) {
+    const target = list[i];
+    str = str.replaceAll(target, '.');
 }
 
-console.log(input.length);
+console.log(str.length);
